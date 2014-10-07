6503294ed7704671b6f1cc6eedc6c89d5e891555
Dominik "Socek" Dlugajczyk <msocek@gmail.com>
2014-10-07 20:37:07 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+# -*- encoding: utf-8 -*-
+from setuptools import setup, find_packages
+
+install_requires = [
+    'hatak>=0.2',
+    'Hatak_Jinja2',
+    'Hatak_Beaker',
+]
+
+if __name__ == '__main__':
+    setup(
+        name='Hatak_Flashmsg',
+        version='0.1',
+        description='Flash Messages plugin for Hatak.',
+        license='Apache License 2.0',
+        packages=find_packages('src'),
+        package_dir={'': 'src'},
+        namespace_packages=['haplugin'],
+        install_requires=install_requires,
+        include_package_data=True,
+        zip_safe=False,
+        package_data={
+            'haplugin': [
+                'flashmsg/templates/*.jinja2'
+            ],
+        }
+    )
